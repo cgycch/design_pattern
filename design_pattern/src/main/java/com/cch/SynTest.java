749afd48e4af4434a5c357a1ebfdde102553faf6
cch <cch@test.com>
2019-03-10 16:13:20 +0800

    CCH test commit

---
+++ design_pattern/src/main/java/com/cch/SynTest.java
@@ -0,0 +1,18 @@
+package com.cch;
+
+public class SynTest {
+
+	public static void main(String[] args) {
+		method1();
+		method2();
+	}
+
+	public static void method1() {
+		System.out.println("cgycch add method 1");
+	}
+
+	public static void method2() {
+		System.out.println("cgycch add method 2");
+	}
+
+}
