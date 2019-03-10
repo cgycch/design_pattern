239c08d5e972e0e2f62ca550b2af31f7cfd05694
Administrator <Administrator@cchPc>
2019-03-10 16:51:23 +0800

    Admin commit update but no sync

--- design_pattern/src/main/java/com/cch/SynTest.java
+++ design_pattern/src/main/java/com/cch/SynTest.java
@@ -8,9 +8,7 @@ public class SynTest {
 	}
 
 	public static void method1() {
-		System.out.println("cgycch add method 1");
-		System.out.println("admin update method 1 hello");
-		System.out.println("cgycch update method 1 again");
+		System.out.println("admin remain method 1");
 	}
 
 	public static void method2() {
