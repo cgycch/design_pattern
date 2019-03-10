4eb9a53eab162f3b989b8d1c6a1108b72522d344
Administrator <Administrator@cchPc>
2019-03-10 17:00:57 +0800

    Admin mark as merged

--- design_pattern/src/main/java/com/cch/SynTest.java
+++ design_pattern/src/main/java/com/cch/SynTest.java
@@ -9,6 +9,8 @@ public class SynTest {
 
 	public static void method1() {
 		System.out.println("admin remain method 1");
+		System.out.println("admin update method 1 again");
+		System.out.println("cgycch update method 1");
 	}
 
 	public static void method2() {
