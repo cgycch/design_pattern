3d37c05c4d0710de76d5b2e4f377a6ea4171e861
Administrator <Administrator@cchPc>
2019-03-10 16:49:40 +0800

    CCH update

--- design_pattern/src/main/java/com/cch/SynTest.java
+++ design_pattern/src/main/java/com/cch/SynTest.java
@@ -3,15 +3,9 @@ package com.cch;
 public class SynTest {
 
 	public static void main(String[] args) {
-		method1();
 		method2();
 	}
 
-	public static void method1() {
-		System.out.println("cgycch add method 1");
-		System.out.println("admin update method 1 hello");
-		System.out.println("cgycch update method 1 again");
-	}
 
 	public static void method2() {
 		System.out.println("cgycch add method 2");
