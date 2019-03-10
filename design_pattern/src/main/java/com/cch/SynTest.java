5193c7609f32ae81982fb2a2daef87be3f9cd5e5
cch <cch@test.com>
2019-03-10 16:18:59 +0800

    CCH update method

--- design_pattern/src/main/java/com/cch/SynTest.java
+++ design_pattern/src/main/java/com/cch/SynTest.java
@@ -9,10 +9,12 @@ public class SynTest {
 
 	public static void method1() {
 		System.out.println("cgycch add method 1");
+		System.out.println("cgycch update method 1");
 	}
 
 	public static void method2() {
 		System.out.println("cgycch add method 2");
+		System.out.println("cgycch update method 2");
 	}
 
 }
